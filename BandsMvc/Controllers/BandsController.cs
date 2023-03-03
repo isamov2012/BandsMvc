a172f15336c7e07a18fd944ed5009f37eca753dc
AnnerleViloan <annerle.enderez@gmail.com>
2023-03-03 16:08:24 +0100

    ---

--- BandsMvc/Controllers/BandsController.cs
+++ BandsMvc/Controllers/BandsController.cs
@@ -15,5 +15,14 @@ namespace BandsMvc.Controllers
             Band[] bands = dataService.GetAllBands();
             return View(bands);
         }
+
+        [HttpGet("/details/{Id}")]
+        public IActionResult Details(int Id)
+        {
+            Band[] bands = dataService.GetAllBands();
+            var q = bands
+                    .First(x=>x.Id == Id);
+            return View(q);
+        }
     }
 }
