6ae4af70305c6b9c96bd2f12e63b748d7854afb4
alexnils <31803475+alexnils@users.noreply.github.com>
2023-03-03 15:37:51 +0100

    add index.cshtml

---
+++ BandsMvc/Controllers/BandsController.cs
@@ -0,0 +1,19 @@
+﻿using BandsMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BandsMvc.Controllers
+{
+    public class BandsController : Controller
+    {
+        DataService dataService = new DataService();
+
+        [HttpGet("/")]
+        [HttpGet("bands/index")]
+        public IActionResult Index()
+        {
+            Band[] bands = dataService.GetAllBands();
+            return View(bands);
+        }
+    }
+}
