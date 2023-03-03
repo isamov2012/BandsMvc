628df1e49b26a89e60cf6b8c598f56e226512998
AnnerleViloan <annerle.enderez@gmail.com>
2023-03-03 14:51:22 +0100

    Added List of Band

---
+++ BandsMvc/Models/DataService.cs
@@ -0,0 +1,13 @@
+﻿namespace BandsMvc.Models
+{
+    public class DataService
+    {
+        List<Band> bands = new List<Band> 
+        
+        {
+            new Band {Id = 5, Name = "Backstreet", Description ="Best Boy Band in the World" },
+            new Band {Id = 10, Name = "Queen", Description ="Best Rock Band in the World" },
+            new Band {Id = 7, Name = "Beatles", Description ="All Times Best Band" },
+        };
+    }
+}
